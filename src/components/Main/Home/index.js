003735cd520735f846c31fe647f8941469516914
Osama-you <osama.w.younis@gmail.com>
2021-01-12 21:08:16 +0200

    Setup Home Component just create home component and add CardContainer tO it Relates #8

---
+++ src/components/Main/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+import CardContainer from '../../common/CardContainer';
+
+const Home = () => {
+  return (
+    <>
+      <CardContainer />
+    </>
+  );
+};
+
+export default Home;
