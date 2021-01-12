707300ab1724648f491175157a18eef2df993d7a
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-01-12 15:27:11 +0200

    addinng the Card container to the main component and adding some comments to what is happening next Relates #13

---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Main.css';
+
+import CardContainer from '../common/CardContainer';
+
+const Main = () => {
+  return (
+    // TODO: router is here
+    <>
+      <CardContainer />
+    </>
+  );
+};
+
+export default Main;
