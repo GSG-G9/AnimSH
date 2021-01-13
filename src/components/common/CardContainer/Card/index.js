9297eb6ca7d1be8968c4719193d8606eb4a01a02
Osama-you <osama.w.younis@gmail.com>
2021-01-13 22:22:45 +0200

    Card Functionality make the card component get the data from props and edit the a tage style Relates #3

--- src/components/common/CardContainer/Card/index.js
+++ src/components/common/CardContainer/Card/index.js
@@ -1,33 +1,40 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 import './Card.css';
 
-const data = {
-  description: ` No Game No Life is a surreal comedy that follows Sora and Shiro,shut-in NEET siblings and the online gamer duo behind the legendaryusername &quot Blank.&quot They view the real world as just anotherlousy game; however, a strange e-mail challenging ...`,
-  name: 'No Game No Life',
-  backgroundImage: 'https://cdn.myanimelist.net/images/anime/5/65187l.webp',
-};
-
-const Card = () => {
-  const { description, name, backgroundImage } = data;
+const Card = (props) => {
+  const { id, name, des, image_url } = props;
   return (
-    <div className="card">
-      <div
-        className="front-card"
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-        }}
-      >
-        <div className="name-box">{name}</div>
-      </div>
-      <div className="back-card">
-        <div className="back-card__container">
-          <h1 className="title">{name}</h1>
-          <p className="description">{description}</p>
+    <div id={id} className="card">
+      <Link to={`/AnimeInfo/${id}`}>
+        <div
+          className="front-card"
+          style={{
+            backgroundImage: `url(${image_url})`,
+          }}
+        >
+          <div className="name-box">{name}</div>
         </div>
-        <div className="rating-box">rating</div>
-      </div>
+        <div className="back-card">
+          <div className="back-card__container">
+            <h3 className="title">{name}</h3>
+            <p className="description">{des}</p>
+          </div>
+          <div className="rating-box">rating</div>
+        </div>
+      </Link>
     </div>
   );
+};
+
+Card.propTypes = {
+  id: propTypes.number.isRequired,
+  name: propTypes.string.isRequired,
+  des: propTypes.string.isRequired,
+  image_url: propTypes.string.isRequired,
 };
 
 export default Card;
