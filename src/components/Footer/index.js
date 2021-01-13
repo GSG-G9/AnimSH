a2c8b0bbb7cf549fbb417f6fa7f4bdb4c8d2044c
Osama-you <osama.w.younis@gmail.com>
2021-01-13 22:42:26 +0200

    Create Footer done from style and html Relates #7

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Footer.css';
 
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <div className="footer">
+      <div className="social-links">
+        <Link to="/">Privacy Policy</Link>
+        <Link to="/">User Agreement</Link>
+      </div>
+      <p>© 2020-2021 AnimeSH Co.,Ltd. All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
