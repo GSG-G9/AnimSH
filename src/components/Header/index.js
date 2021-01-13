13a761fcded157813d727034f751f5295445be14
Alaa Shurrab <alaa.m.shurrab98@gmail.com>
2021-01-13 10:33:50 +0200

    Header Design Update navBar (header) base html structure and adding style to it, Also making it responsive Realtes #6

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
-  return <>Header</>;
-};
+const Header = () => (
+  <nav>
+    <ul>
+      <li className="logo">
+        <img src="./resources/logo.png" alt="" />
+        <h1>AnimSH</h1>
+      </li>
+      <li className="search-icon">
+        <input
+          id="first-name"
+          type="search"
+          name="icon"
+          placeholder="Enter anime name ..."
+        />
+        <div name="icon" className="icon">
+          <span className="fas fa-search" />
+        </div>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Header;
